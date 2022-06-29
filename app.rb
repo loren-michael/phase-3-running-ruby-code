056fbf67c22ec1b1654b7fda96d5674f033a2cca
Loren Hartman <lorenmichaelh@gmail.com>
2022-06-29 13:43:46 -0500

    commit tree ae268adaa1a177b08b192978d7560a638d39b12d

---
+++ app.rb
@@ -0,0 +1,15 @@
+# puts "Hello world!"
+# print "Hello world!"
+# p "Hello world!"
+# pp "Hello world!"
+
+# puts [1, 2, 3, 4];
+# p [1, 2, 3, 4];
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
